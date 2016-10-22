e43a474115ce4b178c7f2a25b0c8ddf55c052751
Eric Seynaeve <github@nosperse.com>
2016-10-22 14:14:04 +0200

    - Adding possibility to flash the image - keeping track of previously detected version - updating some version names -

--- update_fairphone2.sh
+++ update_fairphone2.sh
@@ -20,6 +20,7 @@ set -o pipefail
 set -o nounset
 
 download_dir="/var/tmp/fairphone_downloads"
+sh_file="$HOME/.fairphone2_updatesh"
 declare -A update_url
 declare -A update_md5
 declare -A update_sha2
@@ -35,40 +36,20 @@ function init()
     update_sha2[$version]=$sha2
     update_announce_url[$version]=$announce
   done << EOL
-open_16.08.0 http://storage.googleapis.com/fairphone-updates/fp2-sibon-16.08.0-manual-userdebug.zip 855ee24f97ca85cc3219a9bc67a8967d e2270cf62d507abba87f824e551af10547761c52041b111641235713590407d5 https://forum.fairphone.com/t/fairphone-open-16-08-0-is-now-available/21973
-open_16.09.0 - - - https://forum.fairphone.com/t/fairphone-open-16-09-0-is-now-available/22464
-open_16.10.0 https://storage.googleapis.com/fairphone-updates/d7c72422-62fa-4a19-80af-a2fdd4bee25e/fp2-sibon-16.10.0-manual-userdebug.zip 6481730bc6588507f7b9de63db9c3a67 d2a69742aff49ef00db4a8dcd984fdfc6c9c3723279db21a20a55289d4411e61 https://forum.fairphone.com/t/fairphone-open-16-10-0-is-now-available/22849
+fp2-sibon-16.10.0 https://storage.googleapis.com/fairphone-updates/d7c72422-62fa-4a19-80af-a2fdd4bee25e/fp2-sibon-16.10.0-manual-userdebug.zip 6481730bc6588507f7b9de63db9c3a67 d2a69742aff49ef00db4a8dcd984fdfc6c9c3723279db21a20a55289d4411e61 https://forum.fairphone.com/t/fairphone-open-16-10-0-is-now-available/22849
+fp2-sibon-16.09.0 - - - https://forum.fairphone.com/t/fairphone-open-16-09-0-is-now-available/22464
+fp2-sibon-16.08.0 http://storage.googleapis.com/fairphone-updates/fp2-sibon-16.08.0-manual-userdebug.zip 855ee24f97ca85cc3219a9bc67a8967d e2270cf62d507abba87f824e551af10547761c52041b111641235713590407d5 https://forum.fairphone.com/t/fairphone-open-16-08-0-is-now-available/21973
 fp2-sibon-16.07.1 - - - https://forum.fairphone.com/t/fp-open-os-16-07-is-now-available/21064
 open_16.04.0 - - - https://forum.fairphone.com/t/fairphone-2-open-os-is-available/17208
 EOL
 
-  fp_open_versions=("open_16.04.0" "fp2-sibon-16.07.1" "open_16.08.0" "open_16.09.0" "open_16.10.0")
+  fp_open_versions=("open_16.04.0" "fp2-sibon-16.07.1" "fp2-sibon-16.08.0" "fp2-sibon-16.09.0" "fp2-sibon-16.10.0")
 
   mkdir -p "$download_dir"
-}
-
-function sideload_boot()
-{
-  echo "Connect phone in recovery mode with sideload option on!"
-  echo "Press <enter> to continue"
-  read
-  
-  # Get newest firmware download
-  link=$(lynx --dump $1 'https://fairphone.zendesk.com/hc/en-us/articles/213290023-Fairphone-OS-downloads-for-Fairphone-2'|awk '/http/{print $2}'|grep '.zip'|head -n1)
-
-  echo "-= Downloading update =-"
-  wget $link
-
-  fileName=$(echo $link|rev|cut -d/ -f1|rev)
-  androiddir=$(locate platform-tools|head -n1)
-
-  echo "-= Sideloading update =-"
-  sudo $androiddir/adb sideload $fileName
-
-  echo "-= Removing update =-"
-  sudo rm -f $fileName
-
-  echo "-= Done! Reboot the phone =-"
+  if [[ -f "$sh_file" ]]
+  then
+    source "$sh_file"
+  fi
 }
 
 function phone_information() {
@@ -89,85 +70,109 @@ function phone_information() {
     return 10
   fi
 
-  fp_version=$(adb -s $serial shell "getprop ro.build.display.id" | dos2unix)
-  fp_version=${fp_version##* }
-  echo "-= You are currently running OS version $fp_version =-"
+  if [[ $(adb devices | sed '1d') == *device* ]]
+  then
+    fp_version=$(adb -s $serial shell "getprop ro.build.display.id" | dos2unix)
+    fp_version=${fp_version##* }
+    echo "-= You are currently running OS version $fp_version =-"
+    echo "prev_detected_fp_version=$fp_version" > "$sh_file"
+  else
+    # see if we can find a previous detected version
+    fp_version=${prev_detected_fp_version:-}
+    if [[ -z $fp_version ]]
+    then
+      echo "-= Unable to find current fairphone version. =-"
+      echo "-= Assuming you're running the latest version. =-"
+      fp_version=${fp_open_versions[-1]}
+    fi
+  fi
 }
 
 function download_image() {
   local i=0
+  fp_next_version=''
+  if [[ $fp_version == ${fp_open_versions[-1]} ]]
+  then
+    fp_next_version=$fp_version
+    echo "-= You are running the latest OS version =-"
+    return
+  fi
   while (( i < ${#fp_open_versions[@]} ))
   do
     if [[ ${fp_open_versions[$i]} == $fp_version ]]
     then
+      fp_next_version=${fp_open_versions[$i+1]}
       break
     fi
     i=$(( i + 1 ))
   done
-  if (( i > ${#fp_open_versions[@]} ))
+  if [[ -z $fp_next_version ]]
   then
     echo "-= You are running an unknown OS version =-"
     exit 1
   fi
-  if (( i == ${#fp_open_versions[@]} ))
-  then
-    echo "-= You are running the latest OS version =-"
-    return
-  fi
 
-  fp_next_version=${fp_open_versions[$i+1]}
   local url=${update_url[$fp_next_version]}
-  local filename="$download_dir/${url##*/}"
+  image_filename="$download_dir/${url##*/}"
 
   echo "-= Downloading the Fairphone image for $fp_next_version to $download_dir =-"
 
-  wget --continue --output-document "$filename" $url
+  wget --continue --output-document "$image_filename" $url
 
   echo "-= Checking the integrity of the downloaded image =-"
-  echo "${update_md5[$fp_next_version]} $filename" > "$filename.md5"
-  echo "${update_sha2[$fp_next_version]} $filename" > "$filename.sha2"
+  echo "${update_md5[$fp_next_version]} $image_filename" > "$image_filename.md5"
+  echo "${update_sha2[$fp_next_version]} $image_filename" > "$image_filename.sha2"
   echo -n "MD5 sum check: "
-  md5sum --check "$filename.md5"
+  md5sum --check "$image_filename.md5"
   echo -n "SHA256 sum check: "
-  sha256sum --check "$filename.sha2"
+  sha256sum --check "$image_filename.sha2"
 
   echo "-= All is OK. You can continue to flash this image =-"
 }
 
-function flash_rooted_boot() {
-  echo "Connect phone with usb debugging on!"
-  echo "Press <enter> to continue"
-  read
-  
-  lynx_opts=$1 
+function flash_boot() {
+  if [[ $fp_next_version == $fp_version ]]
+  then
+    echo "-= No need to flash your ROM. =-"
+    return
+  fi
 
-  # Get newest bootimage download
-  link=$(lynx --dump $lynx_opts 'https://fairphone.zendesk.com/hc/en-us/articles/213290023-Fairphone-OS-downloads-for-Fairphone-2'|awk '/http/{print $2}'|grep '.zip'|head -n1)
-  fileName=$(echo $link|rev|cut -d/ -f1|rev)
-  version=$(echo $fileName|cut -d- -f3)
+  echo "-= Please reboot the phone in fastboot mode. =-"
+  echo "   For this:"
+  echo "     - power off the phone"
+  echo "     - disconnect the phone from the computer,"
+  echo "     - startup and hold down the Volume Down button during bootup."
+  echo "       Don't be worried if the bootup screen remains visible. This is normal."
+  echo "   See https://forum.fairphone.com/t/pencil2-installing-fairphone-open-os-using-fastboot-step-by-step-guide/17522"
+  echo "   While rebooting, I will extract the image for version $fp_next_version."
 
-  linkImage=$(lynx --dump $lynx_opts 'https://fp2.retsifp.de/'|awk '/http/{print $2}'|grep "$version/"|head -n1)
-  linkImage=$(lynx --dump $lynx_opts "$linkImage"|awk '/http/{print $2}'|grep '.img'|grep '-eng-'|head -n1)
+  rm -rf "$download_dir/flash"
+  mkdir -p "$download_dir/flash"
+  cd "$download_dir/flash"
+  unzip "$image_filename"
+  chmod u+x "$download_dir/flash/flash.sh"
 
-  echo "-= Downloading root image =-"
-  wget $linkImage
+  echo "-= Preparation is finished. =-"
+  echo "   Ensure that the phone is reconnected via USB to this computer."
+  read -p "Press <enter> when reboot to fastboot is finished and USB is connected again."
 
-  fileName=$(echo $linkImage|rev|cut -d/ -f1|rev)
-  androiddir=$(locate platform-tools|head -n1)
+  if ! (timeout 10 fastboot devices | grep fastboot)
+  then
+    echo "-= No devices detected via fastboot."
+    echo "   Is the connectivity OK ?"
+    echo "      E.g. bad USB cable"
+    echo "      E.g. battery empty"
+    echo "   Did you setup the udev rule correctly ? =-"
+    echo "      Check the setting of the udev rule as described on"
+    echo "      https://developer.android.com/studio/run/device.html#setting-up"
+    echo "      Example of udev rule in the file /etc/udev/rules.d/51-fp2-android.rules:"
+    echo "         SUBSYSTEM==\"usb\", ATTR{idVendor}==\"18d1\", ATTR{idProduct}==\"d00d\",MODE=\"0666\", OWNER=\"your_user_name\""
+    return
+  fi
 
-  echo "-= Reboot phone =-"
-  sudo $androiddir/adb reboot bootloader
-  sleep 30
-
-  echo "-= Push boot.img =-"
-  sudo $androiddir/fastboot flash boot $fileName
-  sleep 10
-
-  echo "-= Reboot phone =-"
-  sudo $androiddir/fastboot reboot
-
-  echo "-= Removing file =-"
-  sudo rm -f $fileName
+  echo "-= Upgrading your OS from $fp_version to $fp_next_version. =-"
+  ./flash.sh
+  echo "-= Done. Your phone should now be rebooting in version $fp_next_version. =-"
 }
 
 function main()
@@ -175,6 +180,7 @@ function main()
   init
   phone_information
   download_image
+  flash_boot
 }
 
 main
