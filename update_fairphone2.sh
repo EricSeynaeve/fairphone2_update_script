10d98896079b4f51cda1136289ed444004a75148
Eric Seynaeve <github@nosperse.com>
2016-10-22 07:21:17 +0200

    - added urls for fairphone open downloads and metadata - added main function for later testing

--- update_fairphone2.sh
+++ update_fairphone2.sh
@@ -19,6 +19,29 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+declare -A update_url
+declare -A update_md5
+declare -A update_sha2
+declare -A update_announce_url
+function init()
+{
+  local version url md5 sha2 announce
+
+  while read version url md5 sha2 announce
+  do
+    update_url[$version]=$url
+    update_md5[$version]=$md5
+    update_sha2[$version]=$sha2
+    update_announce[$version]=$announce
+  done << EOL
+open_16.08.0 http://storage.googleapis.com/fairphone-updates/fp2-sibon-16.08.0-manual-userdebug.zip 855ee24f97ca85cc3219a9bc67a8967d e2270cf62d507abba87f824e551af10547761c52041b111641235713590407d5 https://forum.fairphone.com/t/fairphone-open-16-08-0-is-now-available/21973
+open_16.09.0 - - - https://forum.fairphone.com/t/fairphone-open-16-09-0-is-now-available/22464
+open_16.10.0 https://storage.googleapis.com/fairphone-updates/d7c72422-62fa-4a19-80af-a2fdd4bee25e/fp2-sibon-16.10.0-manual-userdebug.zip 6481730bc6588507f7b9de63db9c3a67 d2a69742aff49ef00db4a8dcd984fdfc6c9c3723279db21a20a55289d4411e61 https://forum.fairphone.com/t/fairphone-open-16-10-0-is-now-available/22849
+open_16.07.1 - - - https://forum.fairphone.com/t/fp-open-os-16-07-is-now-available/21064
+open_16.04.0 - - - https://forum.fairphone.com/t/fairphone-2-open-os-is-available/17208
+EOL
+}
+
 function sideload_boot()
 {
   echo "Connect phone in recovery mode with sideload option on!"
@@ -79,10 +102,11 @@ function flash_rooted_boot() {
   sudo rm -f $fileName
 }
 
-read -p "Do you want to flash a rooted boot image [y/n] ? "
-if [[ $REPLY == y or $REPLY == Y ]]
-then
-  flash_rooted_boot "$@"
-else
-  sideload_boot "$@"
-fi
+function main()
+{
+  init
+}
+
+main
+
+# vim: set ai tabstop=2 shiftwidth=2 expandtab :
